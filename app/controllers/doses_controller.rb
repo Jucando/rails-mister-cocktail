8bed78df62940e25c9e3676561c36ddd4a366818
Julien Chim <julien.chim@gmail.com>
2018-05-17 14:43:28 +0200

    cockails and doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredient = Ingredient.find()
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    # @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render '/cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    render '/cocktails/show'
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,5 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients = JSON.parse(open(url).read)
 
 ingredients["drinks"].each do |ingredient|
-  p ingredient["strIngredient1"]
-  Ingredient.create(name: ingredient["strIngredient1"])
+  Ingredient.create!(name: ingredient["strIngredient1"])
 end
